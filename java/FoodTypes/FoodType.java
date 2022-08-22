11609e8fc0994e560f6a5c02f3514643c748cce7
Amina Salah <aminasalah56@outlook.com>
2022-08-22 19:35:36 +0100

    Testing

--- java/FoodTypes/FoodType.java
+++ java/FoodTypes/FoodType.java
@@ -4,14 +4,20 @@ public abstract class FoodType {
 
     private String name;
     private double price;
-
     private String colour;
     private String origin;
     private String KeptInKitchen;
+    private String isFoodHealthy;
+    private int calories;
+
 
 // Constructor
     public FoodType(String price){
         this.price = 2;
+    }
+
+    public FoodType(int i) {
+
     }
 
 
@@ -24,6 +30,7 @@ public abstract class FoodType {
     }
 
 
+
     public double getPrice(){
         return price = 2;
     }
@@ -32,6 +39,7 @@ public abstract class FoodType {
         this.price = 2;
         return price;
     }
+
 
 
     public String getColour(){
@@ -43,6 +51,7 @@ public abstract class FoodType {
     }
 
 
+
     public String getOrigin(){
         return origin;
     }
@@ -52,11 +61,27 @@ public abstract class FoodType {
 
     }
 
+
+
     public String getKeptInKitchen(){
         return KeptInKitchen;
     }
 
     public void setKeptInKitchen(String keptInKitchen){
         this.KeptInKitchen = keptInKitchen;
+    }
+
+
+
+    public String isFoodHealthy(){
+        return ("I'm a " + this.name + "lets see if i'm healthy!");
+    }
+
+    public int calories(){
+        return calories;
+    }
+
+    public String addSeedless() {
+        return addSeedless();
     }
 }
--- java/FoodTypes/FruitAndVeg.java
+++ java/FoodTypes/FruitAndVeg.java
@@ -1,4 +1,23 @@
 package FoodTypes;
 
-public class FruitAndVeg {
+public class FruitAndVeg extends FoodType {
+
+    private double sugarContent;
+
+    public FruitAndVeg(String price, double sugarContent) {
+        super(price);
+        this.sugarContent = sugarContent;
+    }
+
+
+    // Needs an IF / else statement to print both Y and N
+    @Override
+    public String isFoodHealthy() {
+        return "Yes!";
+    }
+
+    public String addSeedless(){
+        return "No!";
+    }
+
 }
